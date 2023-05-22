7cbaacdfbfb4a69f094de8dae51094e2af41aad6
Enzo Gennari <enzo.gennari02@gmail.com>
2023-05-22 16:26:47 -0300

    Configuracoes redux e requisito 1

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
 import React, { Component } from 'react';
+import { Route, Switch } from 'react-router-dom';
+import Login from './pages/Login';
+import Game from './pages/Game';
+
 import './App.css';
 
 export class App extends Component {
   render() {
     return (
-      <div>App</div>
+      <section>
+        <Switch>
+          <Route exact path="/" component={ Login } />
+          <Route exact path="/game" component={ Game } />
+        </Switch>
+      </section>
     );
   }
 }
---
+++ src/pages/Login.jsx
@@ -0,0 +1,86 @@
+import React, { Component } from 'react';
+import '../styles/login.css';
+
+class Login extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      playerName: '',
+      playerEmail: '',
+      isBtnDisabled: true,
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleClick = this.handleClick.bind(this);
+    this.isBtnDisabled = this.isBtnDisabled.bind(this);
+  }
+
+  handleChange({ target: { name, value } }) {
+    this.setState({
+      [name]: value,
+    }, this.isBtnDisabled);
+  }
+
+  handleClick() {
+    console.log('Submit');
+  }
+
+  isBtnDisabled() {
+    const { playerName, playerEmail } = this.state;
+
+    const regex = /\w+@\w+\.\w{2,8}(\.\w{0,2})?/g;
+    const validateEmail = regex.test(playerEmail);
+    const nameSize = 1;
+    const nameTest = playerName.length >= nameSize;
+
+    this.setState({
+      isBtnDisabled: !(validateEmail && nameTest),
+    });
+  }
+
+  render() {
+    const { playerName, playerEmail, isBtnDisabled } = this.state;
+
+    return (
+      <section className="login-container">
+        <form className="login">
+          <label htmlFor="input-player-name">
+            Nickname
+            <input
+              type="text"
+              data-testid="input-player-name"
+              onChange={ this.handleChange }
+              value={ playerName }
+              name="playerName"
+              id="input-player-name"
+            />
+          </label>
+
+          <label htmlFor="input-gravatar-email">
+            Email
+            <input
+              type="text"
+              data-testid="input-gravatar-email"
+              onChange={ this.handleChange }
+              value={ playerEmail }
+              name="playerEmail"
+              id="input-gravatar-email"
+            />
+          </label>
+
+          <button
+            type="button"
+            disabled={ isBtnDisabled }
+            data-testid="btn-play"
+            onClick={ this.handleClick }
+          >
+            Play
+          </button>
+        </form>
+      </section>
+    );
+  }
+}
+
+export default Login;
