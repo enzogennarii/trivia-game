ee4584be938089f35863cb909b884d32feb51c88
Victor Duque <victor.duque11@hotmail.com>
2023-05-23 14:10:06 -0300

    css do login e header arrumado

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
 import md5 from 'crypto-js/md5';
+import '../styles/Header.css';
 
 function Header() {
   const { email, name } = useSelector((state) => state.login);
@@ -9,16 +10,28 @@ function Header() {
   const gravatarImg = `https://www.gravatar.com/avatar/${hash}`;
 
   return (
-    <div>
-
-      <img src={ gravatarImg } alt="deu ruim" data-testid="header-profile-picture" />
-      <span data-testid="header-player-name">
-        {name}
-      </span>
-      <span data-testid="header-player-email">
-        {email}
-      </span>
-      <span data-testid="header-score">0</span>
+    <div className="header-container">
+      <div className="header">
+        <div>
+          <span className="header-name" data-testid="header-player-name">
+            {name}
+          </span>
+        </div>
+        <img
+          className="header-img"
+          src={ gravatarImg }
+          alt="deu ruim"
+          data-testid="header-profile-picture"
+        />
+        <div>
+          <span className="header-email" data-testid="header-player-email">
+            {email}
+          </span>
+        </div>
+      </div>
+      <div>
+        <span className="header-score" data-testid="header-score">0</span>
+      </div>
     </div>
   );
 }
