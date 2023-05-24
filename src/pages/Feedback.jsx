87104f415be3d65e6505e84907b0947e2b2f7aec
Enzo Gennari <enzo.gennari02@gmail.com>
2023-05-24 15:55:06 -0300

    Concluindo requisito 11

---
+++ src/pages/Feedback.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Feedback extends Component {
+  render() {
+    return (
+      <section className="feedback-page">
+        <p data-testid="feedback-text">Feedback</p>
+      </section>
+    );
+  }
+}
+
+export default Feedback;
